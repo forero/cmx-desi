1e4b99a6fdeac2d9ffa5b41f49fd940cf8d43637
Julien Guy <jguy@lbl.gov>
2020-10-27 08:45:57 -0700

    script

---
+++ flavors.sh
@@ -0,0 +1,2 @@
+python cmx-assign.py --dr dr8 --dtver 0.37.0 --flavor science --obscon dark --obsdate 2020-11-01 --outdir ./test_cmx/ --rundate 2020-03-06T00:00:00 --seed 77 --tileid 7160
+python cmx-assign.py --dr dr8 --dtver 0.37.0 --flavor dithprec --obscon dark --obsdate 2020-11-01 --outdir ./test_cmx/ --rundate 2020-03-06T00:00:00 --seed 77 --tileid 7160
