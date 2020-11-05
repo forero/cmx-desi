89044b064be94b5925744770531a5227dfc84f4c
Jaime E Forero-Romero <j.e.forero.romero@gmail.com>
2020-11-05 15:28:00 -0800

    python scritp

--- generate_fba_cmx_runs.py
+++ generate_fba_cmx_runs.py
@@ -2,19 +2,24 @@ import numpy as np
 
 def print_run(flavor='science', obscon='dark', intileid=307):
     cmx_exec = '~/fiberassign/bin/./fba_cmx'
-    tileid = 1000000 + intileid
+    tileid = intileid
 
     run =  '{} --dr dr8 --dtver 0.43.0 --rundate 2020-03-06T00:00:00 --seed 77 '.format(cmx_exec)
     run += ' --intileid {} ' .format(intileid)
     run += ' --tileid {} '.format(tileid)
     run += ' --obscon {} '.format(obscon)
     run += ' --flavor {} '.format(flavor)
-    run += ' --outdir ./{:06d}_{}_{}'.format(tileid, obscon, flavor)
+    run += ' --outdir ./cmx_fiberassign/{:06d}_{}_{}'.format(tileid, obscon, flavor)
     return run
 
 data = np.loadtxt('cmx_tile_list.txt')
+flavors = ['science', 'dithprec', 'dithlost']
+obscons = ['dark', 'bright']
+
 tileids = np.int_(data[:,0])
-for tileid in tileids:
-    run = print_run(intileid=tileid)
-    print(run)
+for flavor in flavors:
+    for obscon in obscons:
+        for tileid in tileids:
+            run = print_run(obscon=obscon, flavor=flavor, intileid=tileid)
+            print(run)
 
