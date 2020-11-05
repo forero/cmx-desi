1875b1bdabe576756afea9eff7dee19eea1f35e5
Jaime E Forero-Romero <j.e.forero.romero@gmail.com>
2020-11-05 09:22:30 -0800

    ll runs sh

---
+++ all_runs.sh
@@ -0,0 +1,1 @@
+~/fiberassign/bin/./fba_cmx --dr dr8 --dtver 0.43.0 --rundate 2020-03-06T00:00:00 --seed 77  --intileid 307  --tileid 1000307  --obscon dark  --flavor starfaint  --outdir ./1000307_dark_starfaint
